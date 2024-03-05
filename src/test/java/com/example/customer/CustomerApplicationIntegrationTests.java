af0950213cc5de3ca79494f129e78932ee659c3e
Arjen <Arjen.Raap@rabobank.nl>
2024-03-05 11:14:30 +0100

    Corrected comment(2)

--- src/test/java/com/example/customer/CustomerApplicationIntegrationTests.java
+++ src/test/java/com/example/customer/CustomerApplicationIntegrationTests.java
@@ -248,7 +248,7 @@ class CustomerApplicationIntegrationTests {
         assertThat(customersInRepo).hasSizeGreaterThan(0);
         Integer firstCustomerRecordId = customersInRepo.get(0).getId();
 
-        // Check the value of the first record to be set to "bilbo.baggings@hotmail.com"
+        // Check the value of the first record not to be set to "bilbo.baggings@hotmail.com"
         Customer customerBeforeUpdate = customerRepository.findCustomerById(firstCustomerRecordId);
         assertThat(customerBeforeUpdate.getEmail()).isNotEqualTo("bilbo.baggings@hotmail.com");
 
